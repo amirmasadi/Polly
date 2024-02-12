43dfcb638842d69b49c4bfe37f9fbc529fc70079
LENOVO <LENOVO@DESKTOP-G7675F4>
2024-02-12 11:46:44 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using Polly;
+using Polly.CircuitBreaker;
+using Polly.RateLimit;
+using Polly.Retry;
+using Polly.Timeout;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading;
+using System.Xml.Serialization;
+
+// sources:
+// https://www.zenrows.com/blog/c-sharp-polly-retry#implement-an-exponential-backoff-strategy-with-polly-with-csharp
+// https://github.com/App-vNext/Polly
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string url = "https://scrapeme.live/shop/asd";
+        PollyRetryWithPipeline(url);
+    }
+
+    async public static void PollyRetryWithPipeline(string url)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            var optionsComplex = new RetryStrategyOptions<HttpResponseMessage>
+            {
+                ShouldHandle = new PredicateBuilder<HttpResponseMessage>()
+                .HandleResult(r => r.StatusCode == HttpStatusCode.NotFound),
+                MaxRetryAttempts = 4,
+                Delay = TimeSpan.Zero,
+                OnRetry = static args =>
+                {
+                    Console.WriteLine("OnRetry, Attempt: {0}", args.AttemptNumber);
+                    // Event handlers can be asynchronous; here, we return an empty ValueTask.
+                    return default;
+                }
+            };
+
+            var pipeline = new ResiliencePipelineBuilder<HttpResponseMessage>()
+            .AddRetry(optionsComplex)
+            .Build();
+
+            var response = await pipeline.ExecuteAsync(async token => await httpClient.GetAsync(url));
+            Console.WriteLine($"Response: {response.StatusCode}");
+        }
+    }
+}
